2e49e9270bf94cb17744a30fdd5ef1c7dfc7d545
Oleksiy Zelenyuk <o.zelenyuk@pin-up.tech>
2025-01-03 09:47:24 +0100

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,10 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import cloudflare from '@astrojs/cloudflare';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  output: process.env.NODE_ENV === 'production' ? 'server' : 'static',
+  adapter: process.env.NODE_ENV === 'production' ? cloudflare() : undefined,
+});
