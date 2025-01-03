2caa73044a487577a3fe8bfb0b18e9e1201ca70a
Oleksiy Zelenyuk <o.zelenyuk@pin-up.tech>
2025-01-03 16:03:43 +0100

    icons

--- src/components/IconPaths.ts
+++ src/components/IconPaths.ts
@@ -7,7 +7,6 @@
  * 3. Paste the SVG code in your editor.
  * 4. Remove the `<svg>` wrapper so you only have elements like `<path>`, `<circle>`, `<rect>` etc.
  * 5. Remove any `stroke="#000000"` attributes
- * 6. Replace any `fill="#000000"` attributes with `stroke="none"`
  *    (or add `stroke="none"` on shapes with no `fill` or `stroke` specified).
  */
 export const iconPaths = {
@@ -35,4 +34,6 @@ export const iconPaths = {
 	'linkedin-logo': `<rect width="184" height="184" x="36" y="36" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="16" rx="8"/><path fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="16" d="M120 112v64m-32-64v64m32-36a28 28 0 0 1 56 0v36"/><circle stroke="none" cx="88" cy="80" r="12"/>`,
 	'instagram-logo': `<circle cx="128" cy="128" r="40" fill="none" stroke-miterlimit="10" stroke-width="16"/><rect width="184" height="184" x="36" y="36" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="16" rx="48"/><circle cx="180" cy="76" r="12" stroke="none" />`,
 	'tiktok-logo': `<path fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="16" d="M168 106a96 96 0 0 0 56 18V84a56 56 0 0 1-56-56h-40v128a28 28 0 1 1-40-25V89a68 68 0 1 0 80 67Z"/>`,
+	'architecture': `<path fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="16" d="M128 24L24 96l104 72 104-72L128 24zM24 96v64l104 72 104-72V96M128 96v144"/><path fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="16" d="M80 132v-36l48-32 48 32v36"/>`,
+	'robot': `<rect x="64" y="48" width="128" height="96" rx="24" fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="16"/><path fill="none" stroke-linecap="round" stroke-linejoin="round" stroke-width="16" d="M128 24v24M96 144v88M160 144v88M48 192l48-24M160 168l48 24M96 216h64M112 96v4M144 96v4"/>`,
 };
