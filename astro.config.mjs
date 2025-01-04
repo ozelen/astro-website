46ceb3f4d4577722d656df2b62a781177a26f597
Oleksiy Zelenyuk <o.zelenyuk@pin-up.tech>
2025-01-04 19:45:54 +0100

    fix blog images

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,4 +8,9 @@ export default defineConfig({
   output: process.env.NODE_ENV === 'production' ? 'server' : 'static',
   adapter: process.env.NODE_ENV === 'production' ? cloudflare() : undefined,
   // assets: true,
+  image: {
+    service: {
+      entrypoint: 'astro/assets/services/sharp'
+    }
+  }
 });
