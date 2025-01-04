e60191013a8b05eee8a9a2776b42b768e895244f
Oleksiy Zelenyuk <o.zelenyuk@pin-up.tech>
2025-01-04 17:37:06 +0100

    rebrand

--- src/content.config.ts
+++ src/content.config.ts
@@ -14,4 +14,15 @@ export const collections = {
       img_alt: z.string().optional(),
     }),
   }),
+  blog: defineCollection({
+    loader: glob({ base: './src/content/blog', pattern: '**/*.md', }),
+    schema: z.object({
+      title: z.string(),
+      description: z.string(),
+      publishDate: z.coerce.date(),
+      tags: z.array(z.string()),
+      img: z.string(),
+      img_alt: z.string().optional(),
+    }),
+  }), 
 };
