63eb65943d7bc5e39d5baeb25b6874c909337223
Oleksiy Zelenyuk <o.zelenyuk@pin-up.tech>
2025-01-04 20:40:17 +0100

    fix image compression output 2

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,11 +6,14 @@ import cloudflare from '@astrojs/cloudflare';
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: cloudflare(),
+  adapter: cloudflare({
+    mode: 'directory',
+    imageService: true,
+  }),
   image: {
-    service: {
-      entrypoint: '@astrojs/cloudflare/image-service',
-    },
+    service: process.env.NODE_ENV === 'production' 
+      ? { entrypoint: '@astrojs/cloudflare/image-service' }
+      : { entrypoint: 'astro/assets/services/sharp' },
     domains: ['zelenyuk.com'],
     remotePatterns: [{ protocol: "https" }],
   },
